3727098ebfb71dbbf327bb397f72a925760d23b3
Wahbi90 <ben.o.abdelwahab@gmail.com>
2021-03-23 12:33:07 +0000

    table

--- react-snowpack/src/Components/Admin/Student.jsx
+++ react-snowpack/src/Components/Admin/Student.jsx
@@ -1,40 +1,34 @@
 import React from "react";
 import axios from "axios";
 
-let SingleData = []
+let SingleData = [];
 const Student = () => {
-    let initData = axios
-      .get("http://localhost:3000/students/ ")
-      .then((response) => {
-        SingleData = response.data
-        console.log(SingleData, "yoyoy");
+  let initData = axios
+    .get("http://localhost:3000/students/ ")
+    .then((response) => {
+      SingleData = response.data;
+      console.log(SingleData, "yoyoy");
 
-const element = () => {
-        for (let i = 0 ; i < SingleData.lenth ; i++){
-            
-        }
-        }
-      })
-      ;
-
+      const element = () => {
+        for (let i = 0; i < SingleData.lenth; i++) {}
+      };
+    });
   return (
-<div class="container">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>First Name</th>
-        <th>Last Name</th>
-        <th>Age</th>
-        <th>Gender</th>
-        <th>Image</th>
-      </tr>
-    </thead>
-    <tbody id="data">
-
-    </tbody>
-  </table>
-</div>
+    <div class="container">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Age</th>
+            <th>Gender</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody id="data"></tbody>
+      </table>
+    </div>
   );
 };
 export default Student;
