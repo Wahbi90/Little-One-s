37033945f513b9eb5b51a5aedb749b5e7bee8191
Wahbi90 <ben.o.abdelwahab@gmail.com>
2021-03-30 17:21:01 +0000

    erging done

--- react-snowpack/index.jsx
+++ react-snowpack/index.jsx
@@ -2,13 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-<<<<<<< HEAD
 import "./src/firebase/config";
 import "bulma/css/bulma.min.css";
 import store from "./src/store";
-=======
-import "./assets/main.css";
->>>>>>> 10d2a7b506248e473a068c2d3cc9d5e59802fb58
 
 ReactDOM.render(
   <React.StrictMode>
